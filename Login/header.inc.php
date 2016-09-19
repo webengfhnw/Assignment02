77b5a56416f3d1e1eb4227dd92d8909c2db5a602
Andreas Martin <andreas.martin@andreasmartin.ch>
2016-09-19 13:25:09 +0200

    Move login to assignment 04

--- Login/header.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_SESSION['verified'])) {
-    echo "Access denied!";
-    echo "<a href='loginForm.php'> Login </a>";
-    exit();
-}
--- Login/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-include "header.inc.php";
-
-echo "Good day " . $_SESSION['username'];
-echo "<br/>";
-echo "You session ID is: " . session_id();
-echo "<br/><a href='loginForm.php'> logout </a>";
